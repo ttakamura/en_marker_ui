12d392ced79daf424a763384ddeb3bc8a5292a42
Tatsuya Takamura <takamura@tonchidot.com>
2016-04-19 05:23:42 +0900

    App and Build directories

---
+++ app/bootstrap.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import 'babel-polyfill';
+import React    from 'react';
+import ReactDOM from 'react-dom';
+import {Main}   from './components/main';
+
+ReactDOM.render(
+    React.createElement(Main),
+    document.getElementById('app')
+);
---
+++ app/components/main.jsx
@@ -0,0 +1,21 @@
+'use strict';
+
+import React from 'react';
+
+export class Main extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      message: `Hello This is ReactComponent!! 55 ${Date()}`
+    };
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron main">
+          <h1>{this.state.message}</h1>
+        </div>
+      </div>
+    );
+  }
+}
