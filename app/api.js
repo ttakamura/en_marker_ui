eb8335879c34497cd933a4bf77c14a8c319c0911
Tatsuya Takamura <takamura@tonchidot.com>
2016-05-23 06:59:31 +0900

    Convert to Sentence

--- app/api.js
+++ app/api.js
@@ -1,8 +1,25 @@
-import config from './config';
-import { Sentence, Token } from './models/sentence';
+import config                from './config';
+import { Record, Map, List } from 'immutable';
+import { Sentence, Token }   from './models/sentence';
 
 export function convertToSentences(apiResult) {
-
+  return apiResult.sentences.map((s) => {
+    const source = [];
+    let tokens = new List();
+    s.forEach((t) => {
+      if (t.source !== '<bos>' && t.source !== '<eos>') {
+        let token = new Token({ word: t.source });
+        Object.keys(t.annotation).forEach((key) => {
+          if (key !== '<->') {
+            token = token.addAnnotate(key.replace('<', '').replace('>', ''));
+          }
+        });
+        source.push(t.source);
+        tokens = tokens.push(token);
+      }
+    });
+    return new Sentence({ source: source.join(' '), tokens });
+  });
 }
 
 export function predict(text) {
